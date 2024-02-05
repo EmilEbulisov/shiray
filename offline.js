5963c60ed0ffee057b2411485b984365d412c691
EmilEbulisov <146378096+EmilEbulisov@users.noreply.github.com>
2024-02-05 14:24:05 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1707132092,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite3-sheet0.png",
+		"images/shiray-sheet0.png",
+		"images/upbutton-sheet0.png",
+		"images/upbutton-sheet1.png",
+		"images/leftbutton-sheet0.png",
+		"images/leftbutton-sheet1.png",
+		"images/rightbutton-sheet0.png",
+		"images/xbutton-sheet0.png",
+		"images/xbutton-sheet1.png",
+		"images/obutton-sheet0.png",
+		"images/obutton-sheet1.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
