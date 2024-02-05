f83581a4fe2c30df434e5cf0807e51dcd7ec00f1
EmilEbulisov <146378096+EmilEbulisov@users.noreply.github.com>
2024-02-05 23:55:43 +0300

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1707164340,
+	"version": 1707166383,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -15,6 +15,7 @@
 		"images/gamer-sheet0.png",
 		"images/room-sheet0.png",
 		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
